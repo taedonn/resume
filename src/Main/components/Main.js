e690356f778921b5b91574086cafad9ebf372f62
LeeBosa <grandsky95@hotmail.com>
2023-01-17 16:43:06 +0900

    build 추가

---
+++ src/Main/components/Main.js
@@ -0,0 +1,11 @@
+import '../css/Main.css';
+
+function Main() {
+  return (
+    <div className="main">
+
+    </div>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Main from './Main/components/Main';
+import Header from './Main/components/Header';
+import './Main/css/Common.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Header/>
+    <Main/>
   </React.StrictMode>
 );
