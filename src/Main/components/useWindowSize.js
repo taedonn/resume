3aae27a6387f1f88d4b219e019a29388b78d7568
LeeBosa <grandsky95@hotmail.com>
2023-01-18 15:19:59 +0900

    헤더 모바일 추가

--- src/Main/components/useWindowSize.js
+++ src/Main/components/useWindowSize.js
@@ -1,11 +1,11 @@
 import { useState, useLayoutEffect } from 'react';
 
 export const useWindowSize = () => {
-    const [windowSize, setWindowSize] = useState([0,0])
-    const updateWindowSize = () => { setWindowSize([window.innerWidth, window.innerHeight]) }
+    const [windowSize, setWindowSize] = useState([0,0]);
+    const updateWindowSize = () => { setWindowSize([window.innerWidth, window.innerHeight]); }
     useLayoutEffect(() => {
         window.addEventListener('resize', updateWindowSize);
         return () => window.removeEventListener('resize', updateWindowSize);
-    },[])
-    return [windowSize[0], windowSize[1]]
+    },[]);
+    return [windowSize[0], windowSize[1]];
 }
