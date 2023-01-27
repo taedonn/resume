fcb8542a970694446bffae1eac0365a09e9bbb9c
LeeBosa <grandsky95@hotmail.com>
2023-01-27 18:01:44 +0900

    230127

--- src/components/main.js
+++ src/components/main.js
@@ -1,41 +1,48 @@
-import YouTube from 'react-youtube';
-import ScrollEvent from './ScrollEvent';
 import '../css/main.css';
+import "nes.css/css/nes.min.css";
+import pixelCharacter from '../img/pixel_character_ex.webp';
 
 function Main() {
   return (
     <div className="main_index">
-      <div className='index_fixed'>
-        <div className='index_video_wrap'></div>
-        <YouTube
-          className='index_video' 
-          videoId='sbioGKKKa6w' 
-          opts={{
-            playerVars: {
-              autoplay: 1,
-              controls: 0,
-              loop: 1,
-              rel: 0,
-              playlist: 'sbioGKKKa6w'
-            }
-          }}
-          onReady={(e) => { e.target.mute(); e.target.playVideo(); }} 
-          onError={(e) => e.target.reload() }
-        />
-        <ScrollEvent/>
+      <div className='index_left'>
+        <img src={pixelCharacter} alt='픽셀 캐릭터'></img>
       </div>
-      <div className='index_scroll_event'>
-        <section className='index_vision_wrap'>
-          <div className='index_vision'>
-            <h2>VISION</h2>
-            <h3>
-              <span>#프론트엔드</span>
-              <span>#ISTJ</span>
-              <span>#현실주의</span>
-              <span>#포트폴리오</span>
-            </h3>
-          </div>
-        </section>
+      <div className='index_right'>
+        <div className='character nes-container with-title'>
+          <p className='title'>Character</p>
+          <p className='text'>이태호 <span>@xxDevBosa</span></p>
+          <p className='hashtag'>
+            <div className='nes-container is-rounded'><p>#프론트엔드 개발자</p></div>
+            <div className='nes-container is-rounded'><p>#ISTJ</p></div>
+            <div className='nes-container is-rounded'><p>#서울</p></div>
+            <div className='nes-container is-rounded'><p>#게이머</p></div>
+          </p>
+        </div>
+        <div className='skill nes-container with-title'>
+          <p className='title'>Skills</p>
+          <p className='skill_list'>
+            <p class="badge">html</p>
+            <p class="badge">CSS</p>
+            <p class="badge">SCSS</p>
+            <p class="badge">Git</p>
+            <p class="badge">GitHub</p>
+            <p class="badge">JavaScript</p>
+            <p class="badge">jQuery</p>
+            <p class="badge">Node.js</p>
+            <p class="badge">React</p>
+          </p>
+        </div>
+        <div className='works nes-container with-title'>
+          <p className='title'>Works</p>
+          <p className='works_list'>
+            <button className='nes-btn'>@Vision Project</button>
+            <button className='nes-btn'>@RoboMD</button>
+            <button className='nes-btn'>@Astell&Kern</button>
+            <button className='nes-btn'>@Solaseado</button>
+            <button className='nes-btn'>@JejuAir</button>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './components/main';
-import Header from './components/header';
 import './css/common.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
     <Main/>
   </React.StrictMode>
 );
